a4769573e7939535312acd30d8ca15fe68120267
iamEvan <evan@iamevan.dev>
2025-09-13 21:21:27 +0100

    feat(authorization): enhance authorization logic with timing-safe comparison

--- src/modules/authorization.ts
+++ src/modules/authorization.ts
@@ -1,16 +1,38 @@
 import type { Context, Next } from "hono";
 import { authKey, environment } from "./config";
+import { timingSafeEqual } from "node:crypto";
 
 export function isAuthorized(authHeader: string | undefined): boolean {
-  if (environment !== "Testing") {
-    return authHeader === authKey;
+  if (environment === "Testing") {
+    // In testing mode, we always return true.
+    return true;
   }
-  return true;
+
+  // If no auth key is set, then authentication is not needed.
+  if (!authKey) return true;
+
+  if (!authHeader) {
+    // If no auth header is set, then we return false as no authentication method was provided.
+    return false;
+  }
+
+  // Check if the length of the auth header is the same as the auth key.
+  if (authHeader.length !== authKey.length) {
+    return false;
+  }
+
+  // Check if the auth header matches the auth key via timingSafeEqual.
+  const authHeaderBuffer = new TextEncoder().encode(authHeader);
+  const authKeyBuffer = new TextEncoder().encode(authKey);
+  return timingSafeEqual(authHeaderBuffer, authKeyBuffer);
 }
 
 // middleware to check if the user is authorized before allowing access to a route
 export async function authMiddleware(c: Context, next: Next) {
-  if (!isAuthorized(c.req.header("authorization"))) {
+  const authHeader = c.req.header("authorization");
+  const authorized = isAuthorized(authHeader);
+
+  if (!authorized) {
     return c.json({ error: "Unauthorized" }, 401);
   }
 
